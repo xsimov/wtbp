ffc437c04b568f67ee54a126823e9964aa3954ff
xsimov <xsimov@gmail.com>
2014-08-25 15:14:47 +0200

    Added css, popup windows

--- app/assets/javascripts/littleFunctions.js
+++ app/assets/javascripts/littleFunctions.js
@@ -1,8 +1,22 @@
 function toggleDisplay(element){
   if (element.style.display !== 'block'){
     element.style.display = 'block';
+    document.addEventListener('click', goBackOnGreyClick);
     return 'on';
   }
   element.style.display = 'none';
   return null;
+}
+
+function goBackOnGreyClick(event){
+  if (event.target.className === 'greyed'){
+    toggleDisplay(event.target);
+    destroyAllMusiciansList();
+    document.removeEventListener('click', goBackOnGreyClick);
+  }
+}
+
+function destroyAllMusiciansList(){
+  var ul = document.getElementById('all_musicians').children[0].children[0];
+  ul.innerHTML = '';
 }
