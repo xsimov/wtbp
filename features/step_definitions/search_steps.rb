3ecb4171febbf747eb22ede6ff28893a416af25c
xsimov <xsimov@gmail.com>
2014-08-26 20:31:43 +0200

    Fixed some style

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -3,13 +3,15 @@ Given(/^I go to the welcome page$/) do
 end
 
 When(/^I want to search something$/) do
+  @musician = FactoryGirl.create(:musician)
   fill_in "search", with: "kail"
   click_button "Search"
 end
 
 Then(/^I get the results for all fields and kinds$/) do
-  expect(page).to have_text "Results for bands"
-  expect(page).to have_text "Results for musicians"
+  expect(page).to have_text @musician.first_name
+  expect(page).to have_text @musician.last_name
+  expect(page).not_to have_text "Your search didn't come with anything! Sorry :("
 end
 
 Given(/^I go to the bands page$/) do
@@ -35,4 +37,14 @@ end
 Then(/^I get the results for musicians only$/) do
   expect(current_path).to eq("/search/musicians")
   expect(page).to have_text "Kail Laurence"
+end
+
+Given(/^I search for something that doesn't exist$/) do
+  visit root_path
+  fill_in "search", with: "ruru"
+  click_button "Search"
+end
+
+Then(/^I want to see a empty\-thing warning$/) do
+  expect(page).to have_text "Your search didn't come with anything! Sorry :("
 end
