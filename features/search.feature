626a621d4b43dc68d65c5a23c66d858c51b1c2e1
xsimov <xsimov@gmail.com>
2014-08-26 18:17:21 +0200

    Fixed JS turbolinks problem

--- features/search.feature
+++ features/search.feature
@@ -4,3 +4,8 @@ Feature: In a database the search is a must
     Given I go to the welcome page
     When I want to search something
     Then I get the results for all fields and kinds
+
+  Scenario: I search a specific model
+    Given I go to the bands page
+    When I search something
+    Then I get the results for that model only
--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -11,3 +11,15 @@ Then(/^I get the results for all fields and kinds$/) do
   expect(page).to have_text "Results for bands"
   expect(page).to have_text "Results for musicians"
 end
+
+Given(/^I go to the bands page$/) do
+  visit band_path()
+end
+
+When(/^I search something$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I get the results for that model only$/) do
+  pending # express the regexp above with the code you wish you had
+end
