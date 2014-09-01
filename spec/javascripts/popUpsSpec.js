6b7827b70f7f9c5f436ff4f6ba47123afe406268
xsimov <xsimov@gmail.com>
2014-09-01 12:26:17 +0200

    javascript again

--- spec/javascripts/popUpsSpec.js
+++ spec/javascripts/popUpsSpec.js
@@ -1,19 +1,14 @@
-// include spec/javascripts/helpers/some_helper_file.js and app/assets/javascripts/foo.js
 //= require popUpFuncs.js
 
 describe('PopUp functions', function() {
-  
-  it('accepts an options object', function() {
-    var element = document.getElementsByTagName('body')[0];
-    var options = {
-      message: 'Hello',
-      kind: '',
-      father: element
-    };
-    expect(popUp(options)).toEqual(0);
-  });
-
-  it('', function() {
-    
+  context('the htmlCreator', function() {
+    it('returns a div.content containing the message it recieved', function() {
+      var contentDiv = document.createElement('div');
+      var insideParagraph = document.createElement('p');
+      contentDiv.classList.add('content');
+      insideParagraph.innerHTML = 'hello';
+      contentDiv.appendChild(insideParagraph);
+      expect(htmlCreator('hello')).toBe(contentDiv);
+    });
   });
 });
